b2040c3c8da62660816054bcef4a78d125275413
Xavier Sala <utrescu@gmail.com>
2020-06-14 16:46:32 +0200

    Versió Python

---
+++ app.py
@@ -0,0 +1,13 @@
+from LluitadorRandom import LluitadorRandom
+from IRing import IRing
+from Ring import Ring
+
+lluitador1 = LluitadorRandom("DestrossaCervells")
+lluitador2 = LluitadorRandom("MatxacaPilotes")
+
+ring = Ring()
+
+ring.EntradaLluitadors(lluitador1, lluitador2)
+resultat = ring.Lluiteu()
+
+print(resultat[0], "vs", resultat[1])
